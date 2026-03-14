ace11751f1f334c4df954fe2dac21582359361c0
mokese14 <test@keyuandewo@outlook.com>
2026-03-14 16:51:25 +0800

    en

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	auto a = "孩子们,我回来了"
+		cout << a;
+	for (int b = 10; a < 20; a += 1) {
+
+	}
+	cout << "你可以走了";
+	system("pause")
+	return 0;
+}
